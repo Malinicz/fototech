9d4ef18daf9890a7b392145079c4bf98a22c0e2f
Malinicz <malinicz@gmail.com>
2018-09-01 12:01:25 +0200

    amend contentful config

--- src/services/contentfulClient/index.js
+++ src/services/contentfulClient/index.js
@@ -1,10 +1,8 @@
-import tokens from './contentfulTokens';
-
 const contentful = require('contentful');
 
 const client = contentful.createClient({
   space: 'jspcmb9ltjbk',
-  accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN || tokens.deliveryAPI,
+  accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN,
   host: 'cdn.contentful.com',
 });
 
